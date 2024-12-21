0b06a64a4454467c56af00951c0fac743ce8ebe6
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-21 20:00:49 +0100

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -125,3 +125,72 @@ command({
     return message.reply(error);
   }
 });
+
+
+//const { command } = require("../lib");
+
+command(
+  {
+    pattern: "animewall",
+    fromMe: true,
+    desc: "Sends anime wallpapers with multiple CTA buttons",
+    type: "info",
+  },
+  async (message) => {
+    try {
+      // Send slide with anime wallpaper and CTA buttons
+      await message.sendSlide(
+        message.jid,
+        "Nikka Botz Inc",
+        "Anime Wallpapers",
+        message,
+        "Powered by Haki",
+        [
+          [
+            "https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis",
+            "𝞜𝞘𝞙𝞙𝞓",
+            "Get Support",
+            "Powered by Haki",
+            "cta_url",
+            "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L",
+          ],
+          [
+            "https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis",
+            "𝞜𝞘𝞙𝞙𝞓",
+            "Get Support",
+            "Powered by Haki",
+            "cta_url",
+            "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L",
+          ],
+          [
+            "https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis",
+            "𝞜𝞘𝞙𝞙𝞓",
+            "Learn More",
+            "Powered by Haki",
+            "cta_url",
+            "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L",
+          ],
+          [
+            "https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis",
+            "𝞜𝞘𝞙𝞙𝞓",
+            "Get Support",
+            "Powered by Haki",
+            "cta_url",
+            "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L",
+          ],
+          [
+            "https://api.nexoracle.com/wallpapers/anime?apikey=free_key@maher_apis",
+            "𝞜𝞘𝞙𝞙𝞓",
+            "Explore",
+            "Powered by Haki",
+            "cta_url",
+            "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L",
+          ],
+        ]
+      );
+    } catch (error) {
+      console.error("Error in animewall command:", error.message);
+      await message.reply("An error occurred while sending the anime wallpapers.");
+    }
+  }
+);
